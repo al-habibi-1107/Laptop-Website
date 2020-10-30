b2244640ce45171c272023ee7b421231d76b47da
Kamil <kamilanwar2001@gmail.com>
2020-10-30 23:28:10 +0400

    animation done

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+// This timeline allows us to animate diffrent
+// things at diffrent instances of time
+const tl = gsap.timeline({defaults:{ease:"power1.out"}});
 
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+// pattern: [timeline].[the time ]("[name of object]",{animation,duration,stagger(one after the other)})
+// to makes from the initial position already specified to the given position
+// fromto can make both form and to attributes
+tl.to('.text',{y:"0%",duration:1,stagger:0.25});
+tl.to('.slider',{y:"-100%",duration:1.5});
+tl.to('.intro',{y:"-100%",duration:1},"-=1");
+
+tl.fromTo('nav',{opacity:0},{opacity:1,duration: 1});
+tl.fromTo('.big-text',{opacity:0},{opacity:1,duration:1});
